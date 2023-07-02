e81fab6cdf9b83def86af4d3c45c9029f612e88e
chaks64 <chintanbiren.shah@sjsu.edu>
2023-07-02 13:00:33 -0700

    Init commit

---
+++ index.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+// Sample API to test the server
+app.get("/", (req, res) => {
+  const data = {
+    message: "Server running fine",
+  };
+  res.json(data);
+});
+
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server started on Port ${process.env.PORT}`);
+});
